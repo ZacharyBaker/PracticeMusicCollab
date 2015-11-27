820506489e3345e258c2254d3a2a47015935fc51
Zach Baker <zach.r.baker1@gmail.com>
2015-11-27 09:13:02 -0700

    go to update page button works

--- core/public/features/profile/profile.controller.js
+++ core/public/features/profile/profile.controller.js
@@ -1,13 +1,19 @@
 var musicApp = angular.module('musicApp');
 
-musicApp.controller('profileCtrl', ['$scope', 'profileInfo', profileCtrl]);
+musicApp.controller('profileCtrl', ['$scope', 'profileInfo', '$state', profileCtrl]);
 
-function profileCtrl($scope, profileInfo){
+function profileCtrl($scope, profileInfo, $state){
 	
 	$scope.test = 'this is the sauciest test around';
 	
 	console.log(profileInfo);
 	$scope.profileInfo = profileInfo;
-		
+	
+	
+	$scope.goToUpdatePage = function(id){
+		$state.go('update', {
+			_id: id
+		});
+	}
 	
 }
